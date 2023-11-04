00cb28ac1e54e66745e71bdcc27267f0bd892e9b
Ryan Yuen <nashhacking@gmail.com>
2023-11-04 19:49:45 +0800

    still fixing js error

---
+++ src/assets/js/quoteapi.js
@@ -0,0 +1,16 @@
+const url = 'https://quotes15.p.rapidapi.com/quotes/random/';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '755d95763emsh054ecfccbfb050cp1e70dfjsnea335e787a5f',
+		'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}

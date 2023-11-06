48d0d60b82caf660b42db77cfa2000018b58d87b
Ryan Yuen <nashhacking@gmail.com>
2023-11-06 11:43:02 +0800

    html script not syncing with js, but fix api first

--- src/assets/js/quoteapi.js
+++ src/assets/js/quoteapi.js
@@ -1,21 +1,21 @@
-async function fetchQuotes() {
-	const url = 'https://quotes15.p.rapidapi.com/quotes/random/';
-	const options = {
-	  method: 'GET',
-	  headers: {
+async function fetchApi() {
+const url = 'https://quotes15.p.rapidapi.com/quotes/random/';
+const options = {
+	method: 'GET',
+	headers: {
 		'X-RapidAPI-Key': '755d95763emsh054ecfccbfb050cp1e70dfjsnea335e787a5f',
 		'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
-	  }
-	};
-  
-	try {
-	  const response = await fetch(url, options);
-	  const result = await response.text();
-	  console.log(result);
-	} catch (error) {
-	  console.error(error);
 	}
-  }
-  
-  // Call the async function
-  fetchQuotes();
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.json();
+	console.log(result);
+	const quoteAuthor = document.querySelector('.overlay-text__author');
+	quoteAuthor.innerHTML = result.originator.name;
+} catch (error) {
+	console.error(error);
+};
+};
+fetchApi();
